dcce904f013ce842a6f38e5a5820601b17ef0800
saunderscox <saunderscox@gmail.com>
2020-10-08 17:41:15 -0400

    added base animation for editor

---
+++ my-app/public/drilltime.js
@@ -0,0 +1,30 @@
+// cancel action on a function while it's active
+function myMove() {
+    var elem = document.getElementById("p0");   
+    var pos = 0;
+    var id = setInterval(frame, 5);
+    function frame() {
+      if (pos == 350) {
+        clearInterval(id);
+      } else {
+        pos++; 
+        elem.style.top = pos + "px"; 
+        elem.style.left = pos + "px"; 
+      }
+    }
+  }
+  
+  function myMove2() {
+    var elem = document.getElementById("p1");   
+    var pos = 0;
+    var id = setInterval(frame, 5);
+    function frame() {
+      if (pos == 350) {
+        clearInterval(id);
+      } else {
+        pos++; 
+        elem.style.top = pos/2 + "px"; 
+        elem.style.left = pos*2 + "px"; 
+      }
+    }
+  }
