7ec9f29feb1ff8d8b798830159cff7377165e996
saunderscox <saunderscox@gmail.com>
2020-11-11 17:28:20 -0500

    performers can be created and dragged around the field

--- my-app/public/drilltime.js
+++ my-app/public/drilltime.js
@@ -1,137 +1,135 @@
 
-var drill = {
-  performers: [
-    p1,
-    p2,
-    p3,
-    p4,
-    p5,
-    p6
-  ],
-  name: "homegamedrill1"
-};
-
-// cancel action on a function while it's active
-function load() {
-  let sampleInput = { id: 0, inst: "Trumpet" }
-  console.log(sampleInput)
-}
 
 
-function play(pid) {
-  // TODO document.getElementById("button1").disabled = true
-  var elem = document.getElementById(pid);
-  var pos = 0;
-  var id = setInterval(frame, 20);
-  function frame() {
-    if (pos == 350) {
-      clearInterval(id);
-    } else {
-      pos++;
-      elem.style.top = pos + "px";
-      elem.style.left = pos + "px";
-    }
+// function play(pid) {
+//   // TODO document.getElementById("button1").disabled = true
+//   var elem = document.getElementById(pid);
+//   var pos = 0;
+//   var id = setInterval(frame, 20);
+//   function frame() {
+//     if (pos == 350) {
+//       clearInterval(id);
+//     } else {
+//       pos++;
+//       elem.style.top = pos + "px";
+//       elem.style.left = pos + "px";
+//     }
 
-  }
-  setTimeout(function () { enableSubmit(that) }, 1000);
-}
+//   }
+//   setTimeout(function () { enableSubmit(that) }, 1000);
+// }
 
-function myMove2() {
-  var elem = document.getElementById("p1");
-  var pos = 0;
-  var id = setInterval(frame, 5);
-  function frame() {
-    if (pos == 350) {
-      clearInterval(id);
-    } else {
-      pos++;
-      elem.style.top = pos / 2 + "px";
-      elem.style.left = pos * 2 + "px";
-    }
-  }
-}
+// function myMove2() {
+//   var elem = document.getElementById("p1");
+//   var pos = 0;
+//   var id = setInterval(frame, 5);
+//   function frame() {
+//     if (pos == 350) {
+//       clearInterval(id);
+//     } else {
+//       pos++;
+//       elem.style.top = pos / 2 + "px";
+//       elem.style.left = pos * 2 + "px";
+//     }
+//   }
+// }
 
 
-// Chung's Code
-var num = 1;
-var instr = "none";
-var person = { number: num, x: 0, y: 0, instrument: instr };
-var performers = [];
+// // Chung's Code
+// var num = 1;
+// var instr = "none";
+// var person = { number: num, x: 0, y: 0, instrument: instr };
+// var performers = [];
 
-//Create new performer object.
-function createP() {
-  person = { number: num, x: 0, y: 0, instrument: instr };
-  performers.push(person);
-  num++;
-  printOutput(); //Delete later
-}
+// //Create new performer object.
+// function createP() {
+//   person = { number: num, x: 0, y: 0, instrument: instr };
+//   performers.push(person);
+//   num++;
+//   printOutput(); //Delete later
+// }
 
-//Delete most recent performer.
-function deleteP() {
-  performers.pop(person);
-  num--;
-  printOutput(); //Delete later
-}
+// //Delete most recent performer.
+// function deleteP() {
+//   performers.pop(person);
+//   num--;
+//   printOutput(); //Delete later
+// }
 
-//Clears all
-function clearP() {
-  performers = [];
-  num = 1;
-  printOutput(); //Delete later
-}
+// //Clears all
+// function clearP() {
+//   performers = [];
+//   num = 1;
+//   printOutput(); //Delete later
+// }
 
-//Set instrument to clarinet
-function addClarinet() {
-  instr = "clarinet";
-}
-//Set instrument to Trombone
-function addTrombone() {
-  instr = "trombone";
-}
+// //Set instrument to clarinet
+// function addClarinet() {
+//   instr = "clarinet";
+// }
+// //Set instrument to Trombone
+// function addTrombone() {
+//   instr = "trombone";
+// }
 
-//Set most recently added performers instrument to inputted text.
-function setInstrument() {
-  var inst1 = document.getElementById("myText").value;
-  //instr = inst1; //Delete later
-  performers[performers.length - 1]["instrument"] = inst1;
-  printOutput(); //Delete later
-}
+// //Set most recently added performers instrument to inputted text.
+// function setInstrument() {
+//   var inst1 = document.getElementById("myText").value;
+//   //instr = inst1; //Delete later
+//   performers[performers.length - 1]["instrument"] = inst1;
+//   printOutput(); //Delete later
+// }
 
-//Set x coordinate
-function setX(xval) {
-  performers[performers.length - 1]["x"] = xval;
-}
+// //Set x coordinate
+// function setX(xval) {
+//   performers[performers.length - 1]["x"] = xval;
+// }
 
-//Set y coordinate
-function setY(yval) {
-  performers[performers.length - 1]["y"] = yval;
-}
+// //Set y coordinate
+// function setY(yval) {
+//   performers[performers.length - 1]["y"] = yval;
+// }
 
-//Print output. Delete later.
-function printOutput() {
-  var data = "";
-  for (let i = 0; i < performers.length; i++) {
-    data += " " + performers[i]["number"] + " " + performers[i]["instrument"] + ", ";
-  }
-  document.getElementById("demo").innerHTML = data;
-}
+// //Print output. Delete later.
+// function printOutput() {
+//   var data = "";
+//   for (let i = 0; i < performers.length; i++) {
+//     data += " " + performers[i]["number"] + " " + performers[i]["instrument"] + ", ";
+//   }
+//   document.getElementById("demo").innerHTML = data;
+// }
 
 
 // Saunders' Code
-function load2() {
+
+// Initial load
+$("document").ready(function() {
+  // var newText = $("<p>")
+  // newText.append("<h1>Replacement</h1>")
+  // $("#createButton").html(newText);
+
+  $("#createButton").on("click", createPerformer);
+  // $("#createButton").on("mouseleave", whenMouseLeaves);
+  $("#p0").draggable();
+
+
+  function createPerformer() {
+    console.log("Create");
+    $(".container").append("<div id=\"p" + numPerformers + "\" class=\"animate\">" + numPerformers + "</div>")
+    $("#p" + numPerformers).draggable();
+    numPerformers += 1;
+  }
+})
+
+function load() {
   console.log("hello")
 
 }
 
 var pos = 0;
-function createPerformer() {
-  console.log("Create");
-  // $( ".inner" ).append( "<p>Test</p>" );
-  $("#p2").css("left", pos + "px");
-  pos += 50;
-}
+
 function nextSet() {
-  $("#prevSetButton").show();
+
 }
 function prevSet() {
 
